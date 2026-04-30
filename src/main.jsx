66e2920c7033dddfb30812c510bef8177d53ddbe
MahirGandhi <gandhim3@msu.edu>
2026-04-30 11:33:14 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css';
 import './spotlight.css';
 import App from './App.jsx';
 import { CardSpotlight } from './components/CardSpotlight.jsx';
+import { WrenchCursor } from './components/WrenchCursor.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CardSpotlight />
+    <WrenchCursor />
     <App />
   </React.StrictMode>
 );
