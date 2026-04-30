e3229ea86688f274e89bb18df71e5bdadd8f698a
MahirGandhi <gandhim3@msu.edu>
2026-04-30 10:44:12 -0400

    Enable card spotlight effect

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { CardSpotlight } from './components/CardSpotlight.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CardSpotlight />
     <App />
   </React.StrictMode>
 );
