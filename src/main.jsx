054f94d07f3357ddc29d8ad15f086b1f234debac
MahirGandhi <gandhim3@msu.edu>
2026-04-30 10:54:20 -0400

    Import light mode spotlight stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import './spotlight.css';
 import App from './App.jsx';
 import { CardSpotlight } from './components/CardSpotlight.jsx';
 
