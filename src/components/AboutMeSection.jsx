7f73479de9816996ec9e444d087ae30482db5487
MahirGandhi <gandhim3@msu.edu>
2026-04-29 21:33:11 -0400

    Add minimal about me section

---
+++ src/components/AboutMeSection.jsx
@@ -0,0 +1,3 @@
+export function PersonalSection() {
+  return <section id="personal" className="mx-auto max-w-7xl px-5 py-16 lg:px-8"><h2 className="text-4xl font-semibold">More about me</h2></section>;
+}
