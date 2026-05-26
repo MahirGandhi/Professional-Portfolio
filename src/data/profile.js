cc78b74e685ad9ae9a53e7affc675cf2384cad3e
MahirGandhi <gandhim3@msu.edu>
2026-05-26 00:33:18 -0400

    Update profile.js

--- src/data/profile.js
+++ src/data/profile.js
@@ -8,7 +8,7 @@ export const profile = {
   email: "gandhim3@msu.edu",
   phone: "(463)-254-9796",
   linkedin: "https://www.linkedin.com/in/mahir-nishit-gandhi/",
-  resumeUrl: "https://drive.google.com/file/d/1B2HhJeOZlwKcNqxTstyGBj2SYAIed8Fw/view?usp=sharing",
+  resumeUrl: "https://drive.google.com/file/d/1Te_ELuJP3u00GmAYSm2ocs04zNDwWHhv/view?usp=sharing",
   portfolioPdfUrl: "#",
   profileImage: "https://drive.google.com/thumbnail?id=1im1XU7pivk2nPoGgnRnZh4RTLDmG8hvX&sz=w1000",
   heroLine:
